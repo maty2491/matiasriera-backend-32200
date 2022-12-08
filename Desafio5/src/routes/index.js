ef24b0fe1f673070aab5bd9b3ef9fd895e9766d5
Matias Riera <maty_2491@hotmail.com>
2022-12-08 16:39:28 -0300

    Desafio5
    Correccion y hbs en funcionamiento

--- Desafio5/src/routes/index.js
+++ Desafio5/src/routes/index.js
@@ -1,37 +1,50 @@
 import { Router } from "express"
 
 const router = Router()
-const products = []
+const products = [{
+    name: "Windows",
+    price: 100,
+    thumbnail: "https://cdn3.iconfinder.com/data/icons/logos-brands-3/24/logo_brand_brands_logos_excel-512.png"
+}]
 
-router.get("/", (req, res) => {
+ router.get("/ejs", (req, res) => {
     res.render('productForm.ejs')
-}) 
-
-/* router.get("/pug", (req, res) => {
-    res.render('productForm.pug')
-}) 
-
-router.get("/hbs", (req, res) => {
-    res.render('productForm.hbs')   
-}) */
-
-router.get('/productosejs', (req, res) => {        
-    res.render('productos.ejs', {products})
 })
-
-/* router.get('/productospug', (req, res) => {
-
-    res.render('productos.pug', { products })
+router.get('/productosejs', (req, res) => {
+    res.render('productos.ejs', { products })
 })
-
-router.get('/productoshbs', (req, res) => {
-    res.render('productos.hbs', {products})   
-}) */
-
 router.post('/productosejs', (req, res) => {
     const { name, price, thumbnail } = req.body
     products.push({ name, price, thumbnail })
-    res.redirect('/')
+    res.redirect('/ejs')
+}) 
+
+
+router.get("/pug", (req, res) => {
+    res.render('productForm.pug')
+}) 
+router.get('/productospug', (req, res) => {
+    res.render('productos.pug', { products })
+}) 
+router.post('/productospug', (req, res) => {
+    const { name, price, thumbnail } = req.body
+    products.push({ name, price, thumbnail })
+    res.redirect('/pug')
 })
+
+
+router.get("/hbs", (req, res) => {
+    res.render('partials/productForm.hbs')
+})
+router.get('/productoshbs', (req, res) => {
+    res.render('partials/productos.hbs', { products })
+})
+
+router.post('/productoshbs', (req, res) => {
+    const { name, price, thumbnail } = req.body
+    products.push({ name, price, thumbnail })
+    res.redirect('/hbs')
+})
+
 
 export default router
