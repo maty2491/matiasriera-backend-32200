3686836284a4d8411804979068941154d85c3bec
Matias Riera <maty_2491@hotmail.com>
2022-12-12 20:24:59 -0300

    Desafio7

--- Desafio7/src/controllers/productos.js
+++ Desafio7/src/controllers/productos.js
@@ -3,38 +3,56 @@ const { response, request } = require('express')
 const productos = [
     {
         id: 1,
+        codigo: "A1",
         nombre: 'Guitarra Cort Z44',
+        descripcion: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta, modi.",
         precio: 137922.75,
+        stock: 10,
         imagen: 'https://http2.mlstatic.com/D_NQ_NP_632536-MLA48208174035_112021-O.webp'
     },
     {
         id: 2,
+        codigo: "B1",
         nombre: 'Kit De Batería Electrónica Behringer Xd8usb 8 Piezas',
+        descripcion: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta, modi.",
         precio: 174618.73,
+        stock: 10,
         imagen: 'https://http2.mlstatic.com/D_NQ_NP_963956-MLA31621294374_072019-O.webp'
     },
     {
         id: 3,
+        codigo: "C1",
         nombre: 'Bajo Eléctrico 4 Cuerdas Yamaha Trbx174bl',
+        descripcion: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta, modi.",
         precio: 122886.64,
+        stock: 10,
         imagen: 'https://http2.mlstatic.com/D_NQ_NP_728801-MLA46925273219_072021-O.webp'
     },
     {
         id: 4,
+        codigo: "D1",
         nombre: 'Cabezal Para Guitarra Mesa Boogie Dual Rectifier',
+        descripcion: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta, modi.",
         precio: 1119590,
+        stock: 10,
         imagen: 'https://http2.mlstatic.com/D_NQ_NP_615676-MLA31593269319_072019-O.webp'
     },
     {
         id: 5,
+        codigo: "E1",
         nombre: 'Caja Mesa Boogie 4x10 Traditional Power House Made In Usa',
+        descripcion: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta, modi.",
         precio: 708484.76,
+        stock: 10,
         imagen: 'https://http2.mlstatic.com/D_NQ_NP_615314-MLA48110054685_112021-O.webp'
     },
     {
         id: 6,
+        codigo: "E2",
         nombre: 'Caja',
+        descripcion: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta, modi.",
         precio: 15000,
+        stock: 10,
         imagen: 'https://http2.mlstatic.com/D_NQ_NP_615314-MLA48110054685_112021-O.webp'
     }
 ]
@@ -68,12 +86,15 @@ const productoGet = (req = request, res = response) => {
         if (!actualizarProd) {
             return req.status(404).json({ status: "Not Found", data: null })
         }
-        productos.splice(actualizarProd, 1, { nombre, precio, imagen })
+        productos.splice(actualizarProd, 1, { codigo, nombre, descripcion, precio, stock, imagen })
         res.json({
             msg: 'put API - controlador actualizado',
             id,
+            codigo,
             nombre,
+            descripcion,
             precio,
+            stock,
             imagen
         })
     }
@@ -84,8 +105,11 @@ const productoGet = (req = request, res = response) => {
         const newProdId = productos[productos.length - 1].id + 1
         const newProd = {
             id: newProdId,
+            codigo,
             nombre,
+            descripcion,
             precio,
+            stock,
             imagen
         }
 
